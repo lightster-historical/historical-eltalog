d8a77a96ece50eb92a06434689d4af0b18fefcb2
Matt Light <matt.light@lightdatasys.com>
2008-06-17 03:54:10 +0000

    Initial import.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/svn/eltalog@2 0f1480e7-3c37-41b2-a3e2-5f807ab59796

---
+++ core/src/com/lightdatasys/eltalog/Eltalog.java
@@ -0,0 +1,15 @@
+package com.lightdatasys.eltalog;
+
+public class Eltalog 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
